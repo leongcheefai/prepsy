f86863f4f679737b986fa49cdd21632b9aaf8c1d
Leong Chee Fai <leongcheefai0521@gmail.com>
2024-11-18 22:32:12 +0800

    feat: multi-select for profile preferences

--- src/app/profile/preferences/page.tsx
+++ src/app/profile/preferences/page.tsx
@@ -1,6 +1,12 @@
 "use client"
-import { Box, Button, Container, Fieldset, Input, Spacer, Stack } from "@chakra-ui/react";
-import { Field } from "@/components/ui/field";
+import { Box, Button, Container, createListCollection, Fieldset, SelectLabel, Spacer, Stack } from "@chakra-ui/react";
+import {
+    SelectContent,
+    SelectItem,
+    SelectRoot,
+    SelectTrigger,
+    SelectValueText,
+} from "@/components/ui/select"
 
 export default function ProfilePreferences() {
     return (
@@ -15,9 +21,19 @@ export default function ProfilePreferences() {
                 </Stack>
 
                 <Fieldset.Content>
-                    <Field label="Cuisines">
-                        <Input name="cuisines" />
-                    </Field>
+                    <SelectRoot collection={cuisines} size="lg" p={6} multiple>
+                        <SelectLabel>Cuisines</SelectLabel>
+                        <SelectTrigger clearable>
+                            <SelectValueText placeholder="Select your preferred cuisines" />
+                        </SelectTrigger>
+                        <SelectContent bg="white" color="black">
+                            {cuisines.items.map((cuisine) => (
+                                <SelectItem item={cuisine} key={cuisine.value} _hover={{ bg: "black", color: "white" }}>
+                                {cuisine.label}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </SelectRoot>
                 </Fieldset.Content>
                 <Spacer />
 
@@ -29,3 +45,15 @@ export default function ProfilePreferences() {
     </Container>
     );
 }
+
+const cuisines = createListCollection({
+    items: [
+        { value: "italian", label: "Italian" },
+        { value: "french", label: "French" },
+        { value: "german", label: "German" },
+        { value: "chinese", label: "Chinese" },
+        { value: "japanese", label: "Japanese" },
+        { value: "korean", label: "Korean" },
+        { value: "thai", label: "Thai" },
+    ],
+});
